6310fefe094e0dc559b688991ab7cf52d6b0301a
pknowledge <12210239@students.liu.edu.lb>
2025-11-24 21:48:35 +0200

    Reset some settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/dr-online-project/",   // ← ADD THIS LINE
   plugins: [react(), tailwindcss()],
 });
