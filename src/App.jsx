517593293e6ec7bcaedd5432fd34a0330da6413e
pknowledge <12210239@students.liu.edu.lb>
2025-12-31 17:03:02 +0200

    Updated the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
@@ -9,6 +8,18 @@ import Services from "./pages/Services.jsx";
 import Doctors from "./pages/Doctors.jsx";
 import Contact from "./pages/Contact.jsx";
 import Anatomy from "./pages/Anatomy.jsx";
+import DoctorProfile from "./pages/DoctorProfile.jsx";
+import Auth from "./pages/Auth.jsx";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard.jsx";
+
+import AdminLayout from "./pages/admin/AdminLayout.jsx";
+import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import AdminUsers from "./pages/admin/AdminUsers.jsx";
+import AdminMessages from "./pages/admin/AdminMessages";
+import AdminDoctors from "./pages/admin/AdminDoctors.jsx";
+import AdminSpecialties from "./pages/admin/AdminSpecialties";
+import AdminLanguages from "./pages/admin/AdminLanguages.jsx";
+import MyAppointments from "./pages/MyAppointments.jsx";
 
 export default function App() {
   return (
@@ -22,6 +33,19 @@ export default function App() {
           <Route path="/doctors" element={<Doctors />} />
           <Route path="/anatomy" element={<Anatomy />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/doctors/:doctorId" element={<DoctorProfile />} />
+          <Route path="/appointments" element={<MyAppointments />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/doctor/dashboard" element={<DoctorDashboard />} />
+
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="messages" element={<AdminMessages />} />
+            <Route path="doctors" element={<AdminDoctors />} />
+            <Route path="specialties" element={<AdminSpecialties />} />
+            <Route path="languages" element={<AdminLanguages />} />
+          </Route>
         </Routes>
       </main>
       <Footer />
